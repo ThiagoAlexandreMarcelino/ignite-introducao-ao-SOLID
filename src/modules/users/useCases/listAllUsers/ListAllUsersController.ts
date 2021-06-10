115644b77d10ed9eda83bed05badb4729248af1c
Thiago A. Marcelino <thiagodistend@hotmail.com>
2021-06-10 12:11:03 -0300

    Desafio Finalizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from "express";
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase, IRequest } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.headers;
+
+        if (Array.isArray(user_id)) {
+            throw new Error("Id must be a string and not an array");
+        }
+        try {
+            const all = this.listAllUsersUseCase.execute({ user_id });
+
+            return response.status(201).send(all);
+        } catch (error) {
+            return response.status(400).send({ error: error.message });
+        }
+    }
 }
-
 export { ListAllUsersController };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,29 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        console.log(user_id);
+
+        const IsAdmin = this.usersRepository.findById(user_id);
+
+        if (!IsAdmin) {
+            throw new Error("User not Exists");
+        }
+
+        if (!IsAdmin.admin) {
+            throw new Error("Resource only available for admin user");
+        }
+
+        const users = this.usersRepository.list();
+
+        return users;
+    }
 }
 
-export { ListAllUsersUseCase };
+export { ListAllUsersUseCase, IRequest };
